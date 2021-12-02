5ccd4841e47da3a381ff751c776356acbfd7c45e
Erica Stevens <ericastevens99@gmail.com>
2021-12-02 18:42:23 -0500

    Create Swift Forecast model from JSON data using decodable

---
+++ Forecast/Forecast.swift
@@ -0,0 +1,55 @@
+//
+//  Forecast.swift
+//  Forecast
+//
+//  Created by Erica Stevens on 11/27/21.
+//
+
+import Foundation
+
+struct ForecastsResponse: Decodable {
+    var success: Bool
+    var forecasts: [ForecastsResponse.Forecast]
+    
+    enum ResponseCodingKeys: String, CodingKey {
+        case forecasts = "response"
+        case error, success
+    }
+    
+    private struct InnerResponse: Decodable {
+        var periods: [ForecastsResponse.Forecast]
+        var interval: String
+        
+        enum InnerCodingKeys: String, CodingKey {
+            case periods, interval
+        }
+        
+        init(from decoder: Decoder) throws {
+            let innerResponseContainer = try decoder.container(keyedBy: InnerCodingKeys.self)
+            self.interval = try innerResponseContainer.decode(String.self, forKey: .interval)
+            self.periods = try innerResponseContainer.decode([Forecast].self, forKey: .periods)
+        }
+    }
+    
+    struct Forecast: Decodable {
+        var maxTempF: Int
+        var minTempF: Int
+        var icon: String
+        var sunriseISO: String
+        var sunsetISO: String
+        var weather: String
+    }
+    
+    init(from decoder: Decoder) throws {
+        let responseContainer = try decoder.container(keyedBy: ResponseCodingKeys.self)
+        self.success = try responseContainer.decode(Bool.self, forKey: .success)
+        let innerResponse = try responseContainer.decode([ForecastsResponse.InnerResponse].self, forKey: .forecasts)
+        var forecasts: [ForecastsResponse.Forecast] = []
+        for infoDict in innerResponse {
+            for forecast in infoDict.periods {
+                forecasts.append(forecast)
+            }
+        }
+        self.forecasts = forecasts
+    }
+}
--- Forecast/ViewController.swift
+++ Forecast/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
+// Learning Objectives:
+// Learn best way to store private keys & api endpoints
+// Determine best architecture/design pattern for networking class (Coordinator?)
+// Learn to use Codable to extract Swift objects from data ✅
+// Add robust error handling for networking class
+
 class ViewController: UIViewController {
+    
+    private let clientID = "zYYyvxefumON5t3u9JVgB"
+    private let clientSecret = "bMXflvpaACZi1pC0DU2N4KyuDOHDFxp94FsMm18J"
+    private var forecastsEndpoint: String {
+        return "http://api.aerisapi.com/forecasts/11235?client_id=\(clientID)&client_secret=\(clientSecret)"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        guard let endpointURL = URL(string: forecastsEndpoint) else {
+            return
+        }
+        
+        let forecastsRequest = URLRequest(url: endpointURL)
+        
+        let _ = URLSession.shared.dataTask(with: forecastsRequest, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+            
+            guard let data = data,
+                error == nil else {
+                    print(error!)
+                    return
+                }
+            
+            do {
+                let forecastData = try JSONDecoder().decode(ForecastsResponse.self, from: data)
+                for forecast in forecastData.forecasts {
+                    print(forecast)
+                }
+                
+            } catch let error {
+                print(error)
+            }
+        }).resume()
     }
-
-
 }
 
